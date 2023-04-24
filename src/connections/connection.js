1b5adca4a21b934f6b7db76862fe689358d542d5
wlisses <wlissesfernando285@gmail.com>
2023-04-24 13:54:32 -0300

    connection

--- src/connections/connection.js
+++ src/connections/connection.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB_NAME,
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "password",
+  database: process.env.MYSQL_DB_NAME || "db_client_amostra",
 });
 
 module.exports = connection;
