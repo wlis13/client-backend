45e91089056de41434e87490383cb46fe46f5303
wlisses <wlissesfernando285@gmail.com>
2023-04-24 13:20:51 -0300

    foi alterada a forma de verificação do usuárioa existente

--- src/middlewares/userAlready.js
+++ src/middlewares/userAlready.js
@@ -1,12 +1,12 @@
 const connection = require("../connection/connection");
 
 async function validateUserAlreadyExists(req, res, next) {
-  const { email, name } = req.body;
-  const user = await connection.execute(
-'SELECT * FROM users WHERE email = ? OR name = ?',
-  [email, name],
+  const { email } = req.body;
+  const [user] = await connection.execute(
+'SELECT * FROM users WHERE email = ?',
+  [email],
 );
-  if (user) {
+  if (user.length > 0) {
     res.status(409).json({ message: 'User already exists'});
   } else { next(); }
 }
