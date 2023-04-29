b7e770a12a884f967d70b4f6596b3f0167c51d1a
wlisses <wlissesfernando285@gmail.com>
2023-04-29 16:12:31 -0300

    testando função asíncrona

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs/promises');
 const dotenv = require('dotenv');
 const { userRouter } = require('./routes/user.routes');
 const { productsRoutes } = require('./routes/products.routes');
@@ -15,18 +17,23 @@ app.use(cors({
 app.use('/images', express.static(`${__dirname}/images`));
 app.use(express.json());
 
+const PATH_USERS = "../../database/users.json";
+
 app.use('/', userRouter);
 app.use('/', productsRoutes);
 // app.use('/', saleRouter);
 // app.use('/', adminRouter);
 // app.use('/', saleRouter);
-app.get("/test", (_req, res) => {
-  res.status(200).json({ message: "test cors" });
+app.get("/test", async (req, res) => {
+  const { email } = req.body;
+  const fileUsers = path.join(__dirname, PATH_USERS);
+  const dataUsers = JSON.parse(await fs.readFile(fileUsers, "utf-8"));
+  const getUser = dataUsers.find((user) => user.email === email);
+  const token = await generateToken(getUser);
+  res.status(200).json({ token });
 })
 
 const port = process.env.PORT;
 
 app.listen(port);
 console.log(`Api rodando na porta ${port}`);
-
-module.exports = app
