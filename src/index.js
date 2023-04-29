8246afba3ba20904dfdd9c6f86330a40e38280e2
wlisses <wlissesfernando285@gmail.com>
2023-04-29 10:32:42 -0300

    o cors da alegria

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.use(cors(corsOptions));
 
 app.use(function(_req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "x-custom-header");
   next();
 });
 
