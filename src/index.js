12efb0573094b4e10c2b21b78cba374435e4c43e
wlisses <wlissesfernando285@gmail.com>
2023-04-29 16:24:46 -0300

    cors cors cors

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@ const { productsRoutes } = require('./routes/products.routes');
 
 dotenv.config();
 const app = express();
-
+const dominio = ["http://localhost:3000"];
 app.use(cors({
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus : 200
+  origin: function(origin, callback) {
+    if (dominio.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 app.use('/images', express.static(`${__dirname}/images`));
 app.use(express.json());
@@ -25,5 +37,3 @@ const port = process.env.PORT;
 
 app.listen(port);
 console.log(`Api rodando na porta ${port}`);
-
-module.exports = app
