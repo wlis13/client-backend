721e989a145590f22c9e364d5dc857dd25a055ab
wlisses <wlissesfernando285@gmail.com>
2023-04-29 11:31:55 -0300

    cors v3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const { userRouter } = require('./routes/user.routes');
 const { productsRoutes } = require('./routes/products.routes');
+const { loginUserService } = require('./services/user.service');
 
 dotenv.config();
 const app = express();
@@ -14,7 +15,11 @@ app.use(cors({
 app.use('/images', express.static(`${__dirname}/images`));
 app.use(express.json());
 
-app.use('/login', userRouter);
+app.post("/login", async (req, res) => {
+  const userFromReq = req.body;
+  const token = await loginUserService(userFromReq);
+  res.status(200).json(token);
+});
 app.use('/', productsRoutes);
 // app.use('/', saleRouter);
 // app.use('/', adminRouter);
