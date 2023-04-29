303986c1142b220910d919dfd76fa393b5d1d9e5
wlisses <wlissesfernando285@gmail.com>
2023-04-29 16:14:37 -0300

    nada feito

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-const fs = require('fs/promises');
 const dotenv = require('dotenv');
 const { userRouter } = require('./routes/user.routes');
 const { productsRoutes } = require('./routes/products.routes');
@@ -17,23 +15,15 @@ app.use(cors({
 app.use('/images', express.static(`${__dirname}/images`));
 app.use(express.json());
 
-const PATH_USERS = "../../database/users.json";
-
 app.use('/', userRouter);
 app.use('/', productsRoutes);
 // app.use('/', saleRouter);
 // app.use('/', adminRouter);
 // app.use('/', saleRouter);
-app.get("/test", async (req, res) => {
-  const { email } = req.body;
-  const fileUsers = path.join(__dirname, PATH_USERS);
-  const dataUsers = JSON.parse(await fs.readFile(fileUsers, "utf-8"));
-  const getUser = dataUsers.find((user) => user.email === email);
-  const token = await generateToken(getUser);
-  res.status(200).json({ token });
-})
 
 const port = process.env.PORT;
 
 app.listen(port);
 console.log(`Api rodando na porta ${port}`);
+
+module.exports = app
