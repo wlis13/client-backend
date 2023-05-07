5d8cec746ca2dd9f00e9eaa85035cdbc85a7e0c7
wlisses <wlissesfernando285@gmail.com>
2023-05-07 18:59:51 -0300

    nada

--- src/services/salesProducts.service.js
+++ src/services/salesProducts.service.js
@@ -2,33 +2,33 @@ const sale = require('../models/sales.model');
 const saleProduct = require('../models/saleProduct.model');
 const product = require('../models/products.model');
 
-async function findProductsQuantity(id) {
-  const getSaleBySellerId = await sale.findOne({
-    seller_id: id
-  });
+// async function findProductsQuantity(id) {
+//   const getSaleBySellerId = await sale.findOne({
+//     seller_id: id
+//   });
    
-  const getSaleProductBySale = await saleProduct.findOne({
-    sale_id: getSaleBySellerId.id
-  });
+//   const getSaleProductBySale = await saleProduct.findOne({
+//     sale_id: getSaleBySellerId.id
+//   });
   
-  const getProductBySaleProduct = await product.findOne({
-    id: getSaleProductBySale.id
-  });
+//   const getProductBySaleProduct = await product.findOne({
+//     id: getSaleProductBySale.id
+//   });
 
-  const { id, total_price, sale_date } = getSaleBySellerId;
-  const { quantity } = getSaleProductBySale;
-  const { name, price } = getProductBySaleProduct;
+//   const { id, total_price, sale_date } = getSaleBySellerId;
+//   const { quantity } = getSaleProductBySale;
+//   const { name, price } = getProductBySaleProduct;
 
-  const results = {
-    id,
-    total_price,
-    sale_date,
-    name,
-    price,
-    quantity,
-  }
-  return results;
-}
+//   const results = {
+//     id,
+//     total_price,
+//     sale_date,
+//     name,
+//     price,
+//     quantity,
+//   }
+//   return results;
+// }
 
 async function deleteProdSaleService() {
   await saleProduct.deleteMany();
